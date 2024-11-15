3a95ea2767ba2126e56c0f3392662ad1ba77fbd1
Anthony Gallegos <galleg46@uwm.edu>
2024-11-15 12:55:14 -0600

    some styling to get the album cover to be the background, needs more work though

--- client/src/NowPlaying.jsx
+++ client/src/NowPlaying.jsx
@@ -7,6 +7,7 @@ function NowPlaying(props) {
     const [current_track, setCurrent_track] = useState(null);
     const [currentTrackId, setCurrentTrackId] = useState(null);
     const [showTrack, setShowTrack] = useState(false);
+    const [endTimeout, setEndTimeout] = useState(null);
 
     useEffect(() => {
 
@@ -32,9 +33,25 @@ function NowPlaying(props) {
                     
                     // Show track and hide after 10 seconds
                     setShowTrack(true);
-                    setTimeout(() =>  {
-                        setShowTrack(false);
-                    }, 10000);
+                    setTimeout(() =>  setShowTrack(false), 10000);
+
+                    // Clear any exisiting timeouts for the previous track
+                    if (endTimeout) clearTimeout(endTimeout);
+
+                    // Calculate time remaining and schedule the Slide effect to reappeear 
+                    // 15 seconds before the song ends
+                    const songDuraton = data.item.duration_ms;
+                    const songProgress = data.progress_ms;
+                    const timeUntilEnd = songDuraton - songProgress;
+                    const timeToTrigger = timeUntilEnd - 15000;
+
+                    if (timeToTrigger > 0) {
+                        const timeout = setTimeout(() => {
+                            setShowTrack(true);
+                            setTimeout(() => setShowTrack(false), 10000);
+                        }, timeToTrigger);
+                        setEndTimeout(timeout);
+                    }
                 }
 
             } catch (error) {
@@ -48,28 +65,30 @@ function NowPlaying(props) {
         // Poll every 15 seconds to see if the track has changed
         const intervalId = setInterval(fetchNowPlaying, 10000);
         
-        // Clear interval when component unmounts or if the song ID changes
-        return () => clearInterval(intervalId);
+        // Clean up on unmount
+        return () => {
+            clearInterval(intervalId);
+            if (endTimeout) clearTimeout(endTimeout);
+        };
 
     }, [currentTrackId, props.token]);
 
     return (
-        <div>
-            {current_track ? (
-                <div className='main-wrapper'>
-                    <div className='container'>
-                        <Slide direction='down' in={showTrack} mountOnEnter unmountOnExit>
-                            <div>
-                                <img src={current_track.item.album.images[2].url}/>
-                                <p>{current_track.item.name} - {current_track.item.artists[0].name}</p>
-                            </div>
-                        </Slide>
-                    </div>
+
+        <div className='now-playing__container'>
+            <Slide direction='down' in={showTrack} mountOnEnter unmountOnExit>
+                <div
+                    className='now-playing__wrapper'
+                    style={{
+                        backgroundImage: current_track ? `url(${current_track.item.album.images[0].url})` : 'none'
+                    }}
+                >
+                    <img src={current_track.item.album.images[2].url} />
+                    <p>{current_track.item.name} - {current_track.item.artists[0].name}</p>
                 </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+            </Slide>
         </div>
+
     );
 }
 
