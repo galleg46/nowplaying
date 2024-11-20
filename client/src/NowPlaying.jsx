1d25bdd1b6a4eb760caa02a2c1897ae09ccb6873
Anthony Gallegos <galleg46@uwm.edu>
2024-11-20 14:41:19 -0600

    finished look (for now)

--- client/src/NowPlaying.jsx
+++ client/src/NowPlaying.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Slide from '@mui/material/Slide';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import style from '@emotion/styled'
 
 function NowPlaying({token, refreshToken, updateTokens }) {
@@ -81,20 +86,58 @@ function NowPlaying({token, refreshToken, updateTokens }) {
 
     return (
 
-        <div className='now-playing__container'>
-            <Slide direction='down' in={showTrack} mountOnEnter unmountOnExit>
-                <div
-                    className='now-playing__wrapper'
-                    style={{
-                        backgroundImage: current_track ? `url(${current_track.item.album.images[0].url})` : 'none'
-                    }}
-                >
-                    <img src={current_track.item.album.images[2].url} />
-                    <p>{current_track.item.name} - {current_track.item.artists[0].name}</p>
-                </div>
-            </Slide>
+        <div>
+            {current_track ? (
+                <Slide direction='down' in={showTrack} mountOnEnter unmountOnExit>
+                    <Card sx={{ 
+                        display: 'flex',  
+                        width: '52%', 
+                        height: 121,
+                        position: 'relative',
+                        overflow: 'hidden',
+                        borderRadius: 3,
+                        boxShadow: 10}}>
+                        <CardMedia 
+                                component="img" 
+                                src={current_track.item.album.images[0].url}
+                                sx={{ 
+                                    position: 'absolute',
+                                    top: '-20px',
+                                    left: '-20px',
+                                    height: 'calc(100% + 40px)', 
+                                    width: 'calc(100% + 40px)', 
+                                    objectFit: 'cover',
+                                    filter: 'blur(5px)',
+                                    zIndex: 0 }} />
+                        <Box sx={{ 
+                            position: 'relative',
+                            zIndex: 1,
+                            display: 'flex', 
+                            alignItems: 'center',
+                            padding: 1,
+                            background: 'rgba(0, 0, 0, 0.5)',
+                            color: "white",
+                            width: '100%' }}>
+                            <CardMedia 
+                                component="img"
+                                src={current_track.item.album.images[2].url}
+                                sx={{
+                                    width: 80,
+                                    height: 80,
+                                    borderRadius: 1,
+                                    marginRight: 2
+                                }}/>
+                            <Box>
+                                <Typography component="div" variant='h6' sx={{ fontWeight: "bold" }}>{current_track.item.name}</Typography>
+                                <Typography component="div" variant="body2" sx={{ color: 'rgba(255, 255, 255, 0.7' }}>{current_track.item.artists[0].name}</Typography>
+                            </Box>
+                        </Box>
+                    </Card>
+                </Slide>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
-
     );
 }
 
